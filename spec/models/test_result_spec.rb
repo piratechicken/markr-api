e1acdeb66b513aeb380a823109f37d11be649b27
Simon Dwyer <simon@discolabs.com>
2020-06-07 11:27:37 +1000

    Add test result model specs

---
+++ spec/models/test_result_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe TestResult, type: :model do
+  # describe 'uniqueness' do
+  #   it { is_expected.to validate_uniqueness_of(:student_number).scoped_to(:test_id) }
+  # end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:test_id) }
+    it { is_expected.to validate_presence_of(:student_number) }
+    it { is_expected.to validate_presence_of(:student_first_name) }
+    it { is_expected.to validate_presence_of(:student_last_name) }
+    it { is_expected.to validate_presence_of(:marks_available) }
+    it { is_expected.to validate_presence_of(:marks_obtained) }
+  end
+end
