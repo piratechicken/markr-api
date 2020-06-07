c99e27c4c9c247a510b093ec43da145ced3abc58
Simon Dwyer <simon@discolabs.com>
2020-06-07 16:31:57 +1000

    Add tests for invalid xml and update mark obtained

--- spec/requests/test_results_spec.rb
+++ spec/requests/test_results_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe '/test_results', type: :request do
       it 'creates three new TestResults' do
         expect do
           post('/import', params: xml_fixture('simple_test_results'), headers: valid_headers)
+
+          expect(TestResult.all.pluck(:marks_obtained).sort).to eq([9, 10, 13])
         end.to change(TestResult, :count).by(3)
       end
 
@@ -18,44 +20,56 @@ RSpec.describe '/test_results', type: :request do
       end
     end
 
-    # context 'with an unexpected Content-Type header' do
-    #   it 'does not create any new TestResults' do
-    #     pending
-    #   end
+    context 'with an unexpected Content-Type header' do
+      let(:invalid_headers) { { 'Content-Type': 'text/xml' } }
 
-    #   it 'renders a JSON response with expected errors' do
-    #     pending
-    #   end
-    # end
+      it 'does not create any new TestResults' do
+        expect do
+          post('/import', params: xml_fixture('simple_test_results'), headers: invalid_headers)
+        end.to change(TestResult, :count).by(0)
+      end
 
-    # context 'with an unexpected xml body' do
-    #   it 'does not create any new TestResults' do
-    #     pending
-    #   end
+      it 'renders a JSON response with expected errors' do
+        post('/import', params: xml_fixture('simple_test_results'), headers: invalid_headers)
 
-    #   it 'renders a JSON response with expected errors' do
-    #     pending
-    #   end
-    # end
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'Invalid request')
+      end
+    end
 
-    # context 'with a higher test mark for a test result that already exists' do
-    #   it 'updates the test result to the higher mark' do
-    #     pending
-    #   end
+    context 'with an unexpected initial xml key' do
+      it 'does not create any new TestResults' do
+        expect do
+          post('/import', params: xml_fixture('test_result_invalid_key'), headers: valid_headers)
+        end.to change(TestResult, :count).by(0)
+      end
 
-    #   it 'renders a JSON response with errors for the new test_result' do
-    #     pending
-    #   end
-    # end
+      it 'renders a JSON response with expected errors' do
+        post('/import', params: xml_fixture('test_result_invalid_key'), headers: valid_headers)
 
-    # context 'with a lower test mark for a test result that already exists' do
-    #   it 'does not update the test result (leaving it at the higher mark' do
-    #     pending
-    #   end
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'Invalid request')
+      end
+    end
 
-    #   it 'renders a JSON response with errors for the new test_result' do
-    #     pending
-    #   end
-    # end
+    context 'with a higher test mark for a test result that already exists' do
+      before do
+        create(:test_result, student_number: 52158351281, test_id: 1234, marks_obtained: 1)
+      end
+
+      it 'updates the test record with the higher mark' do
+        post('/import', params: xml_fixture('simple_test_results'), headers: valid_headers)
+
+        expect(
+          TestResult.find_by(student_number: 52158351281, test_id: 1234).marks_obtained
+        ).to eq(13)
+      end
+
+      it 'only creates two new records' do
+        expect do
+          post('/import', params: xml_fixture('simple_test_results'), headers: valid_headers)
+        end.to change(TestResult, :count).by(2)
+      end
+    end
   end
 end
