d9252bb2bdad57ccbbbd6f6e83dc43a06503a2d2
Simon Dwyer <simon@discolabs.com>
2020-06-07 11:44:16 +1000

    remove stray space...

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,7 +1,7 @@
 class TestResult < ApplicationRecord
 
   validates :test_id, presence: true
-  validates :student_number, presence: true,  uniqueness: { scope: :test_id }
+  validates :student_number, presence: true, uniqueness: { scope: :test_id }
   validates :student_first_name, presence: true
   validates :student_last_name, presence: true
   validates :marks_available, presence: true
