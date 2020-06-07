70717851c3e917f92b37c8bbd4aaae472e397a68
Simon Dwyer <simon@discolabs.com>
2020-06-07 19:57:19 +1000

    Add route to aggregate

--- spec/routing/test_results_routing_spec.rb
+++ spec/routing/test_results_routing_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe TestResultsController, type: :routing do
     it 'routes to #import' do
       expect(post: '/import').to route_to('test_results#import')
     end
+
+    it 'routes to #aggregate' do
+      expect(get: 'results/1/aggregate')
+        .to route_to(controller: 'test_results', action: 'aggregate', test_id: '1')
+    end
   end
 end
