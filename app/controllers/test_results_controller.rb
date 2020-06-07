c4c4eb67807c00c4aef4b1a637b974f581f46591
Simon Dwyer <simon@discolabs.com>
2020-06-07 20:44:54 +1000

    Use PORO instead of calculating in controller

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -22,35 +22,9 @@ class TestResultsController < ApplicationController
     render(status: :not_found) && return if test_results.blank?
 
     results_as_percentage = test_results.map(&:mark_as_percentage)
+    summary_metrics = ::TestSummaryMetrics.new(test_result_array: results_as_percentage)
 
-    count = results_as_percentage.count
-    sum = results_as_percentage.sum
-    min = results_as_percentage.min
-    max = results_as_percentage.max
-
-    mean = sum / count
-
-    sum_of_squares = results_as_percentage.map { |mark| (mark - mean)**2 }.sum
-    stddev = Math.sqrt(sum_of_squares / (count - 1))
-
-    ordered_results = results_as_percentage.sort
-    p25 = ordered_results[calculate_percentile_index(25, count)]
-    p50 = ordered_results[calculate_percentile_index(50, count)]
-    p75 = ordered_results[calculate_percentile_index(75, count)]
-
-    render(
-      json: {
-        mean: mean.round(1),
-        stddev: stddev.round(1),
-        min: min.round(1),
-        max: max.round(1),
-        p25: p25.round(1),
-        p50: p50.round(1),
-        p75: p75.round(1),
-        count: count.round(1)
-      },
-      status: :ok
-    )
+    render(json: summary_metrics.metrics, status: :ok)
   end
 
   private
@@ -85,11 +59,6 @@ class TestResultsController < ApplicationController
 
     def import_as_hash
       @import_as_hash ||= Hash.from_xml(request.body.read).with_indifferent_access
-    end
-
-    def calculate_percentile_index(percentile, count)
-      # Can floor rank as using for array index
-      (percentile / 100.0 * count).to_i
     end
 
     def validation_errors_to_hash(error)
